5d7aaa1db7286cf2ff2b36ef24dc76776f10d779
JohnMbewe04 <chisomo.mbewe04@gmail.com>
2025-06-23 02:57:08 +0800

    Update DIABETIC.py

--- DIABETIC.py
+++ DIABETIC.py
@@ -21,7 +21,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 
 # Register PDF fonts
 pdfmetrics.registerFont(TTFont("NotoJP", "Midorima-PersonalUse-Regular.ttf"))
-#pdfmetrics.registerFont(TTFont("NotoCN", "/mnt/data/NotoSansCJKtc-Regular.otf"))
+pdfmetrics.registerFont(TTFont("NotoCN", "Source Han Sans CN Light.otf"))
 
 def get_pdf_font(lang_code):
     if lang_code == "ja":
